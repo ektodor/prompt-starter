849fa38d6cf078981d6e9d4cc4ebe0f63f13b215
Jerome Lin <qsc002163@gmail.com>
2025-09-11 15:54:41 +0800

    fix: adjust banner image and responsive design

--- src/views/frontend/SponsorPlan.jsx
+++ src/views/frontend/SponsorPlan.jsx
@@ -18,9 +18,9 @@ export function SponsorPlan() {
             >
               <div className="block lg:flex gap-10">
                 <img 
-                  src="./images/project-2.webp" 
+                  src="./images/sponsor-plan-product.png" 
                   alt="project 2"
-                  className="w-full mb-4 lg-mb-0 lg:w-2/5 h-60 rounded-[10px] object-center flex-auto" 
+                  className="w-full mb-4 lg:mb-0 lg:w-[36.35%] aspect-[16/9] rounded-[10px] object-cover flex-auto" 
                 />
                 <div className="w-full lg:w-3/5 flex-auto">
                   <h2 className="text-h3 lg:text-h3 mb-2 lg:mb-3">AdPrompt Lab</h2>
