8e9de538a2fe0bf233bc90bf0ab9e7cfead8c64c
Jerome Lin <qsc002163@gmail.com>
2026-02-12 14:44:00 +0800

    feat: 新增 InterestTag 組件，提供可選擇的興趣標籤功能

---
+++ src/components/Tag/InterestTag.jsx
@@ -0,0 +1,29 @@
+export function InterestTag({
+	value,
+	name = "interests",
+	defaultChecked = false
+}) {
+	return (
+		<label className="
+			py-3.5
+			border-1 border-neutral-300
+			rounded-xl
+			font-bold text-center
+			cursor-pointer
+			hover:border-primary
+			has-[:checked]:border-primary
+			has-[:checked]:text-primary
+			has-[:checked]:before:content-['✓_']
+			"
+		>
+			<input
+				type="checkbox"
+				name={name}
+				value={value}
+				defaultChecked={defaultChecked}
+				className="hidden"
+			/>
+			{value}
+		</label>
+	);
+}
