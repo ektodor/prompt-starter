0572c2b00d1b5c848accde395761a403f450b8ed
Jerome Lin <qsc002163@gmail.com>
2026-02-12 16:19:26 +0800

    feat: 更新帳戶設定頁面，新增電話、地址、GitHub、LinkedIn 及個人網站輸入欄位，並調整相關樣式

--- src/views/frontend/UserProfile/AccountSettingsTab.jsx
+++ src/views/frontend/UserProfile/AccountSettingsTab.jsx
@@ -41,225 +41,319 @@ export default function AccountSettingsTab({ userProfile }) {
       </div>
       {/* right side setting contect */}
       <div className="lg:col-span-8">
-        <h2
-          className="
-            text-center
-            text-h5 
-            py-3 mb-9
-            rounded-md
-            bg-[linear-gradient(270deg,rgba(255,66,77,0.32)_0%,rgba(255,66,77,0.2)_40.85%,rgba(233,180,14,0.2)_73.09%)]            "
-        >
-          帳戶資訊
-        </h2>
-        {/* 姓名 ＋ Email */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-7">
-          <div>
-            <label htmlFor="userName" className="block font-bold mb-2">
-              姓名
+        {/* account info */}
+        <div className="mb-30">
+          <h2
+            className="
+              text-center
+              text-h5 
+              py-3 mb-9
+              rounded-md
+              bg-[linear-gradient(270deg,rgba(255,66,77,0.32)_0%,rgba(255,66,77,0.2)_40.85%,rgba(233,180,14,0.2)_73.09%)]            "
+          >
+            帳戶資訊
+          </h2>
+          {/* 姓名 ＋ Email */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-7">
+            <div>
+              <label htmlFor="userName" className="block font-bold mb-2">
+                姓名
+              </label>
+              <input
+                id="userName"
+                type="text"
+                defaultValue={userProfile?.display_name}
+                className="
+                  w-full px-3 py-2 
+                  border border-neutral-300
+                  rounded-sm 
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+            </div>
+            <div>
+              <label htmlFor="userEmail" className="block font-bold mb-2">
+                電子郵件
+              </label>
+              <input
+                id="userEmail"
+                type="email"
+                defaultValue={userProfile?.email}
+                placeholder="user@email.com"
+                className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  bg-neutral-100
+                  rounded-sm
+                  text-neutral-500
+                  opacity-60
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+                disabled
+              />
+              <p className="text-text4 text-neutral-300">
+                ＊Email 為​登入​帳號，​無法​自行​修改
+              </p>
+            </div>
+          </div>
+
+          {/* 電話 + 地址 */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-7">
+            <div>
+              <label htmlFor="phone" className="block font-bold mb-2">
+                電話
+              </label>
+              <input
+                id="phone"
+                type="tel"
+                defaultValue={userProfile?.phone}
+                placeholder="0988-123-456"
+                className="
+                  w-full px-3 py-2 
+                  border border-neutral-300
+                  rounded-sm 
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+            </div>
+            <div>
+              <label htmlFor="address" className="block font-bold mb-2">
+                地址
+              </label>
+              <input
+                id="address"
+                type="text"
+                defaultValue={userProfile?.address}
+                placeholder="臺北市大安區新生南路234號"
+                className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+            </div>
+          </div>
+
+          {/* github */}
+          <div className="mb-7">
+            <label htmlFor="github" className="block font-bold mb-2">
+              GitHub
             </label>
             <input
-              id="userName"
-              type="text"
-              defaultValue={userProfile?.display_name}
+              id="github"
+              type="url"
+              defaultValue={userProfile?.github_url}
+              placeholder="https://github.com/username"
               className="
-                w-full px-3 py-2 
-                border border-neutral-300
-                rounded-sm 
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
             />
           </div>
-          <div>
-            <label htmlFor="userEmail" className="block font-bold mb-2">
-              電子郵件
+          {/* linkedin */}
+          <div className="mb-7">
+            <label htmlFor="linkedin" className="block font-bold mb-2">
+              Linkedin
             </label>
             <input
-              id="userEmail"
-              type="email"
-              defaultValue={userProfile?.email}
-              placeholder="user@email.com"
+              id="linkedin"
+              type="url"
+              defaultValue={userProfile?.linkedin_url}
+              placeholder="https://linkedin.com/in/username"
+              className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+            />
+          </div>
+          {/* personalWeb */}
+          <div className="mb-7">
+            <label htmlFor="personalWeb" className="block font-bold mb-2">
+              我的網站
+            </label>
+            <input
+              id="personalWeb"
+              type="url"
+              defaultValue={userProfile?.website_url}
+              placeholder="https://personal-web.com"
+              className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+            />
+          </div>
+
+          {/* Interest */}
+          <div className="mb-7">
+            <p className="mb-3 font-bold">興趣領域（可複選）</p>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-2">
+              {['寫作工具', '設計創作', '商業應用', '程式開發', '娛樂遊戲', '教育學習', '生活應用', '其他'].map((interest) => (
+                <InterestTag key={interest} value={interest} />
+              ))}
+            </div>
+          </div>
+
+          {/* About me */}
+          <div className="mb-22">
+            <label htmlFor="bio" className="block font-bold mb-3">
+              關於我
+            </label>
+            <textarea
+              id="bio"
+              rows="4"
+              value={bio}
+              onChange={(e) => {
+                const newValue = e.target.value;
+                if (newValue.length <= maxLength) {
+                  setBio(newValue)
+                }
+              }}
+              placeholder="熱愛 A​I ​技術​的​創作者，​專精於​寫作​和​設計​相關​的​提示​詞​開發。​希望​透過 AI 工具​幫助​更​多​人​提升​創​作​效率。​"
               className="
                 w-full px-3 py-2
                 border border-neutral-300
-                bg-neutral-100
-                rounded-sm
                 text-neutral-500
-                opacity-60
-                shadow-sm
+                rounded-sm
                 focus:outline-none focus:border-primary
               "
-              disabled
             />
-            <p className="text-text4 text-neutral-300">
-              ＊Email 為​登入​帳號，​無法​自行​修改
+            <p className={`text-right ${isOverLimit ? "text-red-500" : "text-neutral-500"}`}>
+              {bio.length}/100
             </p>
           </div>
-        </div>
 
-        {/* 電話 + 地址 */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-7">
-          <div>
-            <label htmlFor="phone" className="block font-bold mb-2">
-              電話
-            </label>
-            <input
-              id="phone"
-              type="tel"
-              defaultValue={userProfile?.phone}
-              placeholder="0988-123-456"
-              className="
-                w-full px-3 py-2 
-                border border-neutral-300
-                rounded-sm 
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
-            />
-          </div>
-          <div>
-            <label htmlFor="address" className="block font-bold mb-2">
-              地址
-            </label>
-            <input
-              id="address"
-              type="text"
-              defaultValue={userProfile?.address}
-              placeholder="臺北市大安區新生南路234號"
-              className="
-                w-full px-3 py-2
-                border border-neutral-300
-                rounded-sm
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
-            />
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6">
+            <ButtonComponent
+              type="outlined"
+              color="secondary"
+              size="lg"
+              style="w-full col-start-3"
+            >
+              取消變更
+            </ButtonComponent>
+
+            <ButtonComponent
+              type="filled"
+              color="primary"
+              size="lg"
+              style="w-full"
+              clickEvent={() => console.log("saving data")}
+            >
+              儲存變更
+            </ButtonComponent>
           </div>
         </div>
 
-        {/* github */}
-        <div className="mb-7">
-          <label htmlFor="github" className="block font-bold mb-2">
-            GitHub
-          </label>
-          <input
-            id="github"
-            type="url"
-            defaultValue={userProfile?.github_url}
-            placeholder="https://github.com/username"
+        {/* password change */}
+        <div>
+          <h2
             className="
-                w-full px-3 py-2
-                border border-neutral-300
-                rounded-sm
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
-          />
-        </div>
-        {/* linkedin */}
-        <div className="mb-7">
-          <label htmlFor="linkedin" className="block font-bold mb-2">
-            Linkedin
-          </label>
-          <input
-            id="linkedin"
-            type="url"
-            defaultValue={userProfile?.linkedin_url}
-            placeholder="https://linkedin.com/in/username"
-            className="
-                w-full px-3 py-2
-                border border-neutral-300
-                rounded-sm
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
-          />
-        </div>
-        {/* personalWeb */}
-        <div className="mb-7">
-          <label htmlFor="personalWeb" className="block font-bold mb-2">
-            我的網站
-          </label>
-          <input
-            id="personalWeb"
-            type="url"
-            defaultValue={userProfile?.website_url}
-            placeholder="https://personal-web.com"
-            className="
-                w-full px-3 py-2
-                border border-neutral-300
-                rounded-sm
-                text-neutral-500
-                shadow-sm
-                focus:outline-none focus:border-primary
-              "
-          />
-        </div>
-
-        {/* Interest */}
-        <div className="mb-7">
-          <p className="mb-3 font-bold">興趣領域（可複選）</p>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-2">
-            {['寫作工具', '設計創作', '商業應用', '程式開發', '娛樂遊戲', '教育學習', '生活應用', '其他'].map((interest) => (
-              <InterestTag key={interest} value={interest} />
-            ))}
-          </div>
-        </div>
-
-        {/* About me */}
-        <div className="mb-22">
-          <label htmlFor="bio" className="block font-bold mb-3">
-            關於我
-          </label>
-          <textarea
-            id="bio"
-            rows="4"
-            value={bio}
-            onChange={(e) => {
-              const newValue = e.target.value;
-              if (newValue.length <= maxLength) {
-                setBio(newValue)
-              }
-            }}
-            placeholder="熱愛 A​I ​技術​的​創作者，​專精於​寫作​和​設計​相關​的​提示​詞​開發。​希望​透過 AI 工具​幫助​更​多​人​提升​創​作​效率。​"
-            className="
-              w-full px-3 py-2
-              border border-neutral-300
-              text-neutral-500
-              rounded-sm
-              focus:outline-none focus:border-primary
-            "
-          />
-          <p className={`text-right ${isOverLimit ? "text-red-500" : "text-neutral-500"}`}>
-            {bio.length}/100
-          </p>
-        </div>
-
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6">
-          <ButtonComponent
-            type="outlined"
-            color="secondary"
-            size="lg"
-            style="w-full col-start-3"
+              text-center
+              text-h5 
+              py-3 mb-9
+              rounded-md
+              bg-[linear-gradient(270deg,rgba(255,66,77,0.32)_0%,rgba(255,66,77,0.2)_40.85%,rgba(233,180,14,0.2)_73.09%)]            "
           >
-            取消變更
-          </ButtonComponent>
+            變更密碼
+          </h2>
+          <div className="mb-22">
+            <div className="mb-7">
+              <label htmlFor="currentPwd" className="block font-bold mb-2">
+                目前密碼
+              </label>
+              <input
+                id="currentPwd"
+                type="password"
+                defaultValue={userProfile?.password}
+                placeholder="輸入目前密碼"
+                className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+            </div>
+            <div className="mb-7">
+              <label htmlFor="newPwd" className="block font-bold mb-2">
+                新密碼
+              </label>
+              <input
+                id="newPwd"
+                type="password"
+                placeholder="輸入新密碼"
+                className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+              <p className="text-text-5 text-neutral-500">＊密碼需至少8個字符，包含大小寫英文和數字</p>
+            </div>
+            <div className="mb-7">
+              <label htmlFor="checkNewPwd" className="block font-bold mb-2">
+                確認新密碼
+              </label>
+              <input
+                id="checkNewPwd"
+                type="password"
+                placeholder="再次輸入新密碼"
+                className="
+                  w-full px-3 py-2
+                  border border-neutral-300
+                  rounded-sm
+                  text-neutral-500
+                  shadow-sm
+                  focus:outline-none focus:border-primary
+                "
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-x-6">
+            <ButtonComponent
+              type="outlined"
+              color="secondary"
+              size="lg"
+              style="w-full col-start-3"
+            >
+              取消變更
+            </ButtonComponent>
 
-          <ButtonComponent
-            type="filled"
-            color="primary"
-            size="lg"
-            style="w-full"
-            clickEvent={() => console.log("saving data")}
-          >
-            儲存變更
-          </ButtonComponent>
-          
+            <ButtonComponent
+              type="filled"
+              color="primary"
+              size="lg"
+              style="w-full"
+              clickEvent={() => console.log("saving data")}
+            >
+              儲存變更
+            </ButtonComponent>
+          </div>
         </div>
 
       </div>
